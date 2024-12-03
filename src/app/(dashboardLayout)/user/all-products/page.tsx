f11d3cbd1d6843ab776d0f2a8b50544bb79d911f
Mohammad Al Samiul <alsamiul123@gmail.com>
2024-12-03 14:24:34 +0600

    products table partial completed

--- src/app/(dashboardLayout)/user/all-products/page.tsx
+++ src/app/(dashboardLayout)/user/all-products/page.tsx
@@ -1,5 +1,13 @@
+import ProductsTable from "@/components/ui/dashboard/user/ProductsTable";
+import { getAllProducts } from "@/services/ProductService";
 import React from "react";
 
-export default function AllProducts() {
-  return <div>All Products</div>;
+export default async function AllProducts() {
+  const { data: products } = await getAllProducts();
+
+  return (
+    <div>
+      <ProductsTable products={products} />
+    </div>
+  );
 }
